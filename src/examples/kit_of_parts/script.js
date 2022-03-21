3594004f29fd3d48ef3c618c34db204719edfb2f
Naitik Sharma <naitik.sharma@students.iaac.net>
2022-03-21 17:44:25 +0530

    CSS updates

---
+++ src/examples/kit_of_parts/script.js
@@ -0,0 +1,13 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js'
+import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js'
+import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js'
+
+var slider = document.getElementById("numberofparts");
+var output = document.getElementById("numberofparts");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
